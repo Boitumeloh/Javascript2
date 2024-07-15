84525b535a4af4d05ceab5e528a1e321c0a4a180
Boitumelo <boitumelolefophane@gmail.com>
2024-07-15 17:15:44 +0200

    third commit

---
+++ chapter1/ch1_alert.js
@@ -0,0 +1,28 @@
+"use strict";
+
+//alert("say hi from  a different file");
+
+// let mystatus = "new";
+// let scared = true;
+
+// if (mystatus === "new"){
+//     console.log("welcome to Javascript!");
+//     if (scared) {
+//         console.log("Dont worry you will be fine");
+//     }else {
+//         console.log("You have it.You keep on going");
+//     }
+// }
+// else{
+//     console.log("Welcome back, i knew you'd like it");
+// }
+
+// let a = 10; //value is 10
+// console.log(a);//prints 10 into the console
+
+// let feeling = prompt("Hi ,there , How are you doing");
+// console.log(feeling);
+
+console.log(Math.floor(Math.random()));
+console.log(Math.floor(Math.random()*100));
+
